29aef2717fc6d484b56d2f58070bbd2fa1d6ec5b
lalitpatil07bh <lalitrkpatil@gmail.com>
2024-02-28 23:55:43 +0530

    changing some UI of project

--- FrontEnd/src/components/HomeComp.js
+++ FrontEnd/src/components/HomeComp.js
@@ -9,6 +9,7 @@ export default function HomeComp(){
 
 			<div className='image'>
 				<img className="" src={image5} alt="Hospital" width="100%"></img>
+				<h1 id="homeComp">Welcome To LifeLine Hospital</h1>
 				
 			</div>
 			
